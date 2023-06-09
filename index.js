887518aa0c698abf5fb2980df81f9c15fcd4404a
Mystree <72470852+MystreeGit@users.noreply.github.com>
2023-06-09 01:52:40 -0400

    Added UI by refactoring previous code into a button and text which allows you to both play the game and see the results

--- index.js
+++ index.js
@@ -3,70 +3,96 @@ let paper = "paper";
 let scissors = "scissors";
 let draw = "draw";
 let win = "win";
-let lost = "lost"
+let lost = "lost";
+
+document.getElementById("btnRock").addEventListener("click", function () {
+  playRound(rock, getComputerChoice());
+});
+
+document.getElementById("btnPaper").addEventListener("click", function () {
+  playRound(paper, getComputerChoice());
+});
+
+document.getElementById("btnScissors").addEventListener("click", function () {
+  playRound(scissors, getComputerChoice());
+});
+
+const resultText = document.getElementById("result");
 
 function getRandomNumber(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 function getComputerChoice() {
-    let computerChoice = null;
+  let computerChoice = null;
 
-    switch (getRandomNumber(3)) {
-        case 0:
-            computerChoice = rock;
-            break;
-        case 1:
-            computerChoice = paper;
-            break;
-        case 2:
-            computerChoice = scissors;
-            break;
-    }
+  switch (getRandomNumber(3)) {
+    case 0:
+      computerChoice = rock;
+      break;
+    case 1:
+      computerChoice = paper;
+      break;
+    case 2:
+      computerChoice = scissors;
+      break;
+  }
 
-    return computerChoice;
+  return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-
-    // Make sure to convert playerSelection to all lowercase before this
-    if (playerSelection == computerSelection) {return draw;} else {
-        switch (playerSelection) {
-            case rock:
-                if (computerSelection == paper) {return win;} else if (computerSelection == scissors) {return lost;} 
-                break;
-                
-            case paper:
-                if (computerSelection == rock) {return win;} else if (computerSelection == scissors) {return lost;} 
-                break;
-            
-            case scissors:
-                if (computerSelection == paper) {return win;} else if (computerSelection == rock) {return lost;} 
-                break;
-            
-            default:
-                console.log("ERROR!: Player did not input valid choice");
-                break;
+  // Make sure to convert playerSelection to all lowercase before this
+  let playerRoundOutcome;
+  if (playerSelection == computerSelection) {
+    playerRoundOutcome = draw;
+  } else {
+    switch (playerSelection) {
+      case rock:
+        if (computerSelection == paper) {
+          playerRoundOutcome = win;
+        } else if (computerSelection == scissors) {
+          playerRoundOutcome = lost;
         }
-    }
+        break;
 
+      case paper:
+        if (computerSelection == rock) {
+          playerRoundOutcome = win;
+        } else if (computerSelection == scissors) {
+          playerRoundOutcome = lost;
+        }
+        break;
+
+      case scissors:
+        if (computerSelection == paper) {
+          playerRoundOutcome = win;
+        } else if (computerSelection == rock) {
+          playerRoundOutcome = lost;
+        }
+        break;
+
+      default:
+        console.log("ERROR!: Player did not input valid choice");
+        break;
+    }
+  }
+
+  resultText.innerHTML = playerRoundOutcome;
 }
 
+// function game() {
+//     let playerWins = 0;
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = prompt("Input rock, paper, or scissors");
+//         playerSelection = playerSelection.toLowerCase();
+//         let computerSelection = getComputerChoice();
 
+//         playerRoundOutcome = playRound(playerSelection, computerSelection);
+//         if (playerRoundOutcome == win) { playerWins += 1; }
+//         console.log("Player has " + playerRoundOutcome);
+//     }
+//     console.log("Game Over! Player Score is " + playerWins);
+// }
 
-
-function game() {
-    let playerWins = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Input rock, paper, or scissors");
-        playerSelection = playerSelection.toLowerCase();
-        let computerSelection = getComputerChoice();
-        
-        playerRoundOutcome = playRound(playerSelection, computerSelection);
-        if (playerRoundOutcome == win) { playerWins += 1; }
-        console.log("Player has " + playerRoundOutcome);
-    }
-    console.log("Game Over! Player Score is " + playerWins);
-}
-
-game();
+// game();
